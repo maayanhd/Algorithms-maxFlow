2386cb2a7ad4d66e123367668eaf4e7f4cca0482
Gralux <50963220+Gralux@users.noreply.github.com>
2020-08-05 11:40:07 +0300

    dsa

--- Algorithms-MaxFLow/FileInputHandling.cpp
+++ Algorithms-MaxFLow/FileInputHandling.cpp
@@ -1,63 +1,76 @@
 #include "FileInputHandling.h"
-
+#include <sstream>
 FlowNetworkGraph* FileInputHandling::InitializeGraphFromFile(ifstream& graphData)
 {
-     FlowNetworkGraph* resGraph = nullptr;
+	FlowNetworkGraph* resGraph = nullptr;
+	stringstream inputStreamStr;
+	if (graphData.is_open())
+	{
 
-     if (graphData.is_open())
-     {
-          int n, m, u, v, s, t, c;
+		int n, m, u, v, s, t, c;
 
-          ReadIntFromStream(graphData, n);
-          ReadIntFromStream(graphData, m);
-          ReadIntFromStream(graphData, s);
-          ReadIntFromStream(graphData, t);
-          
-          resGraph = new FlowNetworkGraph(n, s, t);
-          
-          int itr = 0;
-          for (int i = 0; i < m; i++, itr++)
-          {
-               ReadIntFromStream(graphData, u);
-               ReadIntFromStream(graphData, v);
-               ReadIntFromStream(graphData, c);
-               resGraph->AddEdge(u, v, c);
-          }
-          
-          // Making sure all characters has been read
-          char lastCharacterToClean;
-          graphData >> lastCharacterToClean;
+		inputStreamStr = GetStringStreamFromFile(graphData);
+		ReadIntFromStream(inputStreamStr, n);
+		inputStreamStr = GetStringStreamFromFile(graphData);
+		ReadIntFromStream(inputStreamStr, m);
+		inputStreamStr = GetStringStreamFromFile(graphData);
+		ReadIntFromStream(inputStreamStr, s);
+		inputStreamStr = GetStringStreamFromFile(graphData);
+		ReadIntFromStream(inputStreamStr, t);
 
-          // Assuring we finished read the file at this point as we should have
-          if (!graphData.eof())
-          {
-               NotifyInputError();
-          }
-     }
-     else
-     {
-          cout << "Error: File not found" << endl;
-          exit(1);
-     }
+		resGraph = new FlowNetworkGraph(n, s, t);
 
-     return resGraph;
+		int itr = 0;
+		for (int i = 0; i < m; i++, itr++)
+		{
+			inputStreamStr = GetStringStreamFromFile(graphData);
+			ReadIntFromStream(inputStreamStr, u);
+			ReadIntFromStream(inputStreamStr, v);
+			ReadIntFromStream(inputStreamStr, c);
+			resGraph->AddEdge(u, v, c);
+		}
+
+		// Making sure all characters has been read
+		char lastCharacterToClean;
+		graphData >> lastCharacterToClean;
+
+		// Assuring we finished read the file at this point as we should have
+		if (!graphData.eof())
+		{
+			NotifyInputError();
+		}
+	}
+	else
+	{
+		cout << "Error: File not found" << endl;
+		exit(1);
+	}
+
+	return resGraph;
 }
 
-void FileInputHandling::ReadIntFromStream(ifstream& graphData, int& inputVal)
+stringstream FileInputHandling::GetStringStreamFromFile(ifstream& graphData)
 {
-     graphData >> inputVal;
+	string inputStr;
+	getline(graphData, inputStr);
 
-     if (graphData.fail())
-     {
-          NotifyInputError();
-     }
+	return stringstream(inputStr);
+}
+void FileInputHandling::ReadIntFromStream(stringstream& streamStr, int& inputVal)
+{
+	streamStr >> inputVal;
 
-     //Clearing error flags
-     graphData.clear();
+	if (streamStr.fail())
+	{
+		NotifyInputError();
+	}
+
+	//Clearing error flags
+	streamStr.clear();
 }
 
 void FileInputHandling::NotifyInputError()
 {
-     cout << "invalid input" << endl;
-     exit(1);
+	cout << "invalid input" << endl;
+	exit(1);
 }
